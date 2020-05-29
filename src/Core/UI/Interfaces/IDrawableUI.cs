2c762d014c6167a51cd6c0a27de7d02364ef1ec1
xePixTvx <haslinger-mario@gmx.at>
2020-05-30 01:14:08 +0200

    TESTING

--- src/Core/UI/Interfaces/IDrawableUI.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Core.UI.Interfaces
-{
-    interface IDrawableUI<Type> : IDrawable<Type>
-    {
-        void setOrigin(string align_x = "left", string align_y = "top");
-        void setPosition(string align_x = "left", string align_y = "top", float x_pos = 0, float y_pos = 0);
-    }
-}
